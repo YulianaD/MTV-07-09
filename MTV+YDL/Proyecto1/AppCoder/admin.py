c04beed79bb44d48a6426870a70e97b797b45013
YulianaD <112353620+YulianaD@users.noreply.github.com>
2022-09-13 20:55:51 -0300

    Delete MTV+YDL/Proyecto1 directory

--- MTV+YDL/Proyecto1/AppCoder/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import familiares
-
-
-admin.site.register(familiares)
--- MTV+YDL/Proyecto1/AppCoder/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class familiares(models.Model):
-    nombre = models.CharField(max_length=50)
-    edad = models.IntegerField()
-    fecha_de_nacimiento = models.DateField()
